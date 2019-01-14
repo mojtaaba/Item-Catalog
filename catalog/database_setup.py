14f1b9497e508d63266a10c9f0d589c201132f41
mojtaba alamer <37722752+mojtaaba@users.noreply.github.com>
2019-01-14 16:48:47 +0300

    fixed PEP8

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -6,47 +6,51 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'user'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
-	picture = Column(String(250))
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 
 class Category(Base):
-	__tablename__ = 'category'
+    __tablename__ = 'category'
 
-	id = Column(Integer, primary_key = True)
-	name = Column(String(80), nullable = False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-	    return {
-	    	'id': self.id,
-	        'name': self.name,
-	    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
 
 class CategoryItem(Base):
-	__tablename__ = 'category_item'
+    __tablename__ = 'category_item'
 
-	name = Column(String(80), nullable = False)
-	description = Column(String(250))
-	id = Column(Integer, primary_key = True)
-	category_id = Column(Integer, ForeignKey('category.id'))
-	category = relationship(Category)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    name = Column(String(80), nullable=False)
+    description = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-	    return {
-	        'category': self.category.name,
-	        'description': self.description,
-	        'name': self.name,
-	    }
+    @property
+    def serialize(self):
+        return {
+            'category': self.category.name,
+            'description': self.description,
+            'name': self.name,
+        }
+
 
 engine = create_engine('sqlite:///Catalog.db')
 Base.metadata.create_all(engine)
